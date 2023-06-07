8af6f550b8e128f41c25458f51fa458bea770f31
Gajendra saini <sainigj86@gmail.com>
2023-06-07 05:43:12 -0700

    fgdf

---
+++ routes/admin.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const Admin = require("../model/admin");
+
+const adminRouter = express.Router();
+
+adminRouter.post("/signup-admin", async (req, res) => {
+  try {
+    const { name, email, mobile, password } = req.body;
+
+    const existingUser = await Admin.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ msg: "admin with same email already exists!" });
+    }
+
+    // const hashedPassword = await bcryptjs.hash(password, 8);
+
+    let admin = new Admin({
+      name,
+      email,
+      mobile,
+      password,
+    });
+    admin = await admin.save();
+    res.json(admin);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+adminRouter.post("/signin-admin", async (req, res) => {
+  try {
+  const { email, password } = req.body;
+
+  const admin = await Admin.findOne({ email });
+  console.log(admin);
+  if (!admin) {
+      return res
+      .status(400)
+      .json({ msg: "admin with this email does not exist!" });
+  }
+
+  // const isMatch = await bcryptjs.compare(password, user.password);
+  if (password != admin.password) {
+      return res.status(400).json({ msg: "Incorrect password." });
+  }
+  res.json(admin);
+
+  } catch (e) {
+  res.status(500).json({ error: e.message });
+  }
+});
+
+
+module.exports = adminRouter;
---
+++ routes/user.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const User = require("../model/user");
+
+const userRouter = express.Router();
+
+userRouter.post("/signup-user", async (req, res) => {
+    try {
+      const { name, email, mobile, password } = req.body;
+  
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res
+          .status(400)
+          .json({ msg: "User with same email already exists!" });
+      }
+  
+  
+      let user = new User({
+        name,
+        email,
+        mobile,
+        password,
+      });
+      user = await user.save();
+      res.json(user);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  });
+
+userRouter.post("/signin-user", async (req, res) => {
+    try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    console.log(user);
+    if (!user) {
+        return res
+        .status(400)
+        .json({ msg: "User with this email does not exist!" });
+    }
+
+    // const isMatch = await bcryptjs.compare(password, user.password);
+    if (password != user.password) {
+        return res.status(400).json({ msg: "Incorrect password." });
+    }
+    res.json(user);
+
+    } catch (e) {
+    res.status(500).json({ error: e.message });
+    }
+});
+
+module.exports = userRouter;
